b0a4625e2e941bfbcaac4398a9661fae3da85fb2
Ashlyn Karan <ashlynk@gmail.com>
2017-05-19 14:32:16 -0500

    add proj03

---
+++ proj03/proj03.py
@@ -0,0 +1,15 @@
+# Name:
+# Date:
+
+
+""" 
+proj 03: Guessing Game
+
+Generate a random number between 1 and 9 (including 1 and 9). 
+Ask the user to guess the number, then tell them whether they guessed too low, too high, 
+or exactly right. Keep the game going until the user types exit.
+Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
+"""
+
+
