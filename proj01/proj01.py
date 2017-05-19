a74a28b6a7248a4d8c312b99bab32ff1d33fc9a2
Ashlyn Karan <ashlynk@gmail.com>
2017-05-19 12:59:43 -0500

    Changed formatting for comments

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,10 +1,10 @@
-## Name:
-## Date:
+# Name:
+# Date:
 
-## proj01: A Simple Program
-## This program asks the user for his/her name and age.
-## Then, it prints a sentence that says when the user will turn 100.
+# proj01: A Simple Program
+# This program asks the user for his/her name and age.
+# Then, it prints a sentence that says when the user will turn 100.
 
-## If you complete extensions, describe your extensions here!
+# If you complete extensions, describe your extensions here!
 
 
